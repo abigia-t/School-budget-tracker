c663a751614a5605283c3bafcd1d456603ff8d10
yekiber <yekiberk@gmail.com>
2025-02-25 12:20:03 +0300

    xyz

---
+++ SBT_frontend/src/pages/GM/RFH.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const RFH = () => {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold">Resource and Finance Head</h1>
+    </div>
+  );
+};
+
+export default RFH;
