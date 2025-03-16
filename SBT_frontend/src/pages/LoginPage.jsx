07f99f8b0342f184415815db10efdbddef846963
ayeleasefa <ayele9626@gmail.com>
2025-03-16 09:55:40 -0400

    login redirected

--- SBT_frontend/src/pages/LoginPage.jsx
+++ SBT_frontend/src/pages/LoginPage.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import AuthForm from "../components/AuthForm";
 import NavBar from "../components/NavBar";
+import { useNavigate } from "react-router-dom";
 const LoginPage = () => {
+  const navigate = useNavigate();
+
+  const handleLogin = (role) => {
+    navigate("/parent");
+  };
   return (
     <div>
-     <NavBar/>
+      <NavBar />
       <div className="flex justify-center  items-center py-6 pt-10">
-        <AuthForm />
+        <AuthForm onLoginSuccess={handleLogin} />
       </div>
     </div>
   );
